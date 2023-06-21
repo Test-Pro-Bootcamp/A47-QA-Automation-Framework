e1b66623fa00d97a45d2eb125c6467a599851057
velodark <122947109+velodark@users.noreply.github.com>
2023-06-21 17:00:35 -0400

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -17,7 +17,7 @@ public class LoginTests extends BaseTest {
 
         //
 
-        
+
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
