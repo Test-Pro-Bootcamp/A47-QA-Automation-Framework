014326a6086715a81ce07e9c026ea2b7eb08e269
cll2 <75150662+cll2@users.noreply.github.com>
2023-06-22 20:33:04 -0400

    registrationNavigation()
    created a new class, Homework 16. Created a test to confirm navigation to registration link

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -1,26 +1,30 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import java.time.Duration;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
+public class Homework16 extends BaseTest {
 
-public class LoginTests extends BaseTest {
     @Test
-    public void LoginEmptyEmailPasswordTest() {
-//      ***test changes in order to fork***
-//      Added ChromeOptions argument below to fix websocket error
+    public void registrationNavigation() {
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications");
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://qa.koel.app/";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+        WebElement registrationLink = driver.findElement(By.cssSelector("[id='hel']"));
+        registrationLink.click();
+        WebElement registerButton = driver.findElement(By.cssSelector("[value='Register']"));
+        Assert.assertTrue(registerButton.isDisplayed());
 
 
     }
