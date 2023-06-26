670034ff8bcffd12015aa080372ae7f728338a6c
angel <raptfuneral@gmail.com>
2023-06-25 19:51:28 -0700

    update

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -51,4 +52,20 @@ public class BaseTest {
         submit.click();
     }
 
+    void clickNextSong() throws InterruptedException{
+        WebElement nextSong = driver.findElement(By.cssSelector("i.next.fa.fa-step-forward.control"));
+        nextSong.click();
+        Thread.sleep(2000);
+    }
+
+    void play() throws InterruptedException{
+        WebElement play = driver.findElement(By.cssSelector("span.play"));
+        play.click();
+        Thread.sleep(3000);
+    }
+
+    void songValidation(){
+        WebElement bars = driver.findElement(By.cssSelector("img[alt='Sound bars']"));
+        Assert.assertTrue(bars.isDisplayed());
+    }
 }
--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -2,10 +2,14 @@ import org.testng.annotations.Test;
 
 public class Homework18 extends BaseTest{
     @Test
-    public void playSong(){
+    public void playSong() throws InterruptedException{
         openLoginUrl();
         enterEmail("angel.ayala@testpro.io");
         enterPassword("school!sc0");
         clickSubmit();
+        clickNextSong();
+        play();
+        songValidation();
     }
+
 }
