626a45e4648f51dc752f346a7f3bb2d9c094d6f5
jasmynmedina <101167480+jasmynmedina@users.noreply.github.com>
2023-06-27 17:12:36 -0700

    Homework16
    update

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -5,12 +5,13 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Homework16 {
+public class Homework16 extends BaseTest {
     @Test
     public void registrationNavigation() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
         driver.get("https://qa.koel.app/");
         driver.findElement(By.id("hel")).click();
         String url = "https://qa.koel.app/registration.php";
