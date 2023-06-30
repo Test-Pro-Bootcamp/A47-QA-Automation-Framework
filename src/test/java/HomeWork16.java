83a4c9a66e70ced159f5640d18ac4a7b2f13def2
CourtMatt <courtney.matthews01@testpro.io>
2023-06-30 17:57:05 -0500

    assertion
    correction made to assertion code

---
+++ src/test/java/HomeWork16.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class HomeWork16 extends BaseTest {
+    @Test
+    public void registrationNavigation() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        WebElement registrationButton= driver.findElement(By.cssSelector("[id='hel']"));
+        registrationButton.click();
+
+        String registrationUrl = "https://qa.koel.app/register";
+        Assert.assertEquals(driver.getCurrentUrl(),registrationUrl);
+
+        driver.quit();
+    }
+}
