6913ddb69422f1ff7e9ae5ce6563da3199ab02c2
Matt Pierce <119891174+mattpierce017@users.noreply.github.com>
2023-07-01 18:43:12 -0400

    Update Homework19.java

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 public class Homework19 extends BaseTest {
 @Test
     public void deletePlaylist() throws InterruptedException {
-    
+
     //get url
     //login email
     //login password
