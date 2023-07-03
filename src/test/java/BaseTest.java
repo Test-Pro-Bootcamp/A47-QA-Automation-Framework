c6c5daac016ba45bf6f50a7a7647fa686bea9fdf
Matt Pierce <119891174+mattpierce017@users.noreply.github.com>
2023-07-03 16:32:29 -0400

    updated hw file

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,9 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
 import java.util.*;
 
 import java.time.Duration;
@@ -14,6 +16,7 @@ import java.time.Duration;
 public class BaseTest {
 
     public static WebDriver driver = null;
+    public static WebDriverWait wait = null;
     public static String url = "https://qa.koel.app/";
 
     @BeforeSuite
@@ -21,13 +24,15 @@ public class BaseTest {
         WebDriverManager.chromedriver().setup();
     }
     @BeforeMethod
-    public void launchBrowser() throws InterruptedException{
+    @Parameters ({"BaseURL"})
+    public void launchBrowser(String BaseURL) throws InterruptedException{
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        Thread.sleep(5000);
+        url = BaseURL;
+        driver.get(url);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
     }
     @AfterMethod
@@ -35,8 +40,14 @@ public class BaseTest {
 
         driver.quit();
     }
+    @DataProvider (name="CorrectLoginProviders")
+    public static Object[][] getDataFromDataProviders(){
+        return new Object[][] {
+                {"matt.pierce@testpro.io", "te$t$tudent"}
+        };
+    }
 
-    protected static void openLoginUrl() {
+    public static void openLoginUrl() {
         String url = "https://qa.koel.app/";
         driver.get(url);
     }
@@ -51,18 +62,84 @@ public class BaseTest {
     }
 
     protected static void enterPassword(String password) throws InterruptedException{
-        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
-        passwordInput.click();
+
+        WebElement passwordInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='password']")));
         passwordInput.clear();
         //inputs valid password for login into password field
         passwordInput.sendKeys(password);
-        Thread.sleep(5000);
     }
 
     public void clickSubmit() throws InterruptedException{
-        WebElement loginButton = driver.findElement(By.cssSelector("[type='submit']"));
+        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable((By.cssSelector("[type='submit']"))));
         loginButton.click();
-        Thread.sleep(5000);
     }
 
+    @Test
+    public void addSongToPlaylist(String email, String password) throws InterruptedException{
+        String newSongAddedNotificationText = "Added 1 song into";
+        //open url
+        //openLoginUrl();
+        //input email
+        enterEmail(email);
+        //input password
+        enterPassword(password);
+        //click submit
+        clickSubmit();
+
+        //search for song
+        searchForSong("Dark Days");
+        //click View All Btn
+        clickViewAllBtn();
+        // verify on search results page
+        verifySearchUrl();
+        //select the first song returned in the search
+        selectFirstSongResult();
+        //click Add To Btn
+        clickAddToBtn();
+        //choose Playlist to add the song to
+        choosePlaylist();
+        //checking for success message notification
+        Assert.assertTrue((getNotificationText().contains(newSongAddedNotificationText)));
+
+
+    }
+
+    public void choosePlaylist() throws InterruptedException{
+        WebElement testPlaylist = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"songResultsWrapper\"]/header/div[3]/div/section[1]/ul/li[15]")));
+        testPlaylist.click();
+    }
+
+    public void clickAddToBtn() throws InterruptedException {
+        WebElement addTo = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='btn-add-to']")));
+        addTo.click();
+    }
+
+    public void selectFirstSongResult() throws InterruptedException{
+        WebElement firstResult = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@id=\"songResultsWrapper\"]//tr[contains(@class,'song-item')])[1]")));
+        firstResult.click();
+    }
+
+    public void clickViewAllBtn() throws InterruptedException{
+        WebElement viewAllBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@data-test='view-all-songs-btn']")));
+        viewAllBtn.click();
+
+    }
+
+    public void verifySearchUrl() throws InterruptedException {
+        WebElement songResultsWrapper = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//section[@id='songResultsWrapper']")));
+        Assert.assertTrue(songResultsWrapper.isDisplayed());
+    }
+
+    public void searchForSong(String song) throws InterruptedException{
+        WebElement searchField = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='search']")));
+        searchField.click();
+        searchField.clear();
+        searchField.sendKeys(song);
+
+    }
+
+    public String getNotificationText () {
+        WebElement notificationElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.alertify-logs.top.right")));
+        return notificationElement.getText();
+    }
 }
