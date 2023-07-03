6312ae83935e70f4ba2c61359e4ab451a5c0bc97
velodark <122947109+velodark@users.noreply.github.com>
2023-07-03 17:23:28 -0400

    Update README.md

---
+++ src/test/java/homework18.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class homework18 extends BaseTest {
+    @Test
+    public void playSong() {
+
+
+    }
+}
