888c811828af607702b256b799b9789127de9619
angel <raptfuneral@gmail.com>
2023-07-03 18:00:02 -0700

    update

---
+++ src/test/java/Homework21.java
@@ -0,0 +1,19 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework21 extends BaseTest{
+    String message ="Updated playlist";
+
+    @Test(dataProvider = "validCredentials")
+    public void renamingPlaylist(String email, String password){
+        enterEmail(email);
+        enterPassword(password);
+        clickSubmit();
+        selectPlaylist();
+        selectEdit();
+        renamePlaylist("something NEW");
+        validateAction();
+
+        Assert.assertTrue(validateAction().contains(message));
+    }
+}
