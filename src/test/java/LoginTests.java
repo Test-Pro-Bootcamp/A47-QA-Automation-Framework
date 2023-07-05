3a78a89a6faa5281dae96616123dd1b5b028b13d
Ice-testpro <irene.perdon@testpro.io>
2023-07-05 10:43:35 -0500

    Test case for Registration
    Create a test case in the registration Navigation using @Test annotation.

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -17,11 +19,13 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://testpro.io/";
+        String url = "https://qa.koel.app//";
         driver.get(url);
+
+        WebElement registrationNavigation = driver.findElement(By.cssSelector("[type='submit']"));
+        registrationNavigation.click();
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
 
-        
     }
 }
