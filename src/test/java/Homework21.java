0f2539d37aa5b19620175ed18ea4a67adfad3654
Matt Pierce <119891174+mattpierce017@users.noreply.github.com>
2023-07-05 21:00:41 -0400

    Updated Homework21 file

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -1,2 +1,59 @@
-public class Homework21 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework21 extends BaseTest {
+    @Test (dataProvider = "CorrectLoginProviders", dataProviderClass = BaseTest.class)
+    public void renamePlaylist (String email, String password) throws InterruptedException {
+
+        String deletedPlaylistMessage = "Deleted playlist";
+        //get url
+        //login email
+        enterEmail(email);
+        //login password
+        enterPassword(password);
+        //click submit
+        clickSubmit();
+        //choose playlist
+        openPlaylist();
+        // click delete that playlist
+        clickDeleteBtn();
+
+        //gets deletion text
+        getDeletionText();
+
+        // verifies that the Deletion Message contains the deletion text.
+        Assert.assertTrue((getDeletionText().contains(deletedPlaylistMessage)));
+    }
+
+    private void openPlaylist() {
+        WebElement openedPlaylist = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"playlists\"]/ul/li[5]/a")));
+        openedPlaylist.click();
+    }
+
+
+    public String getDeletionText() {
+        WebElement deletePlaylistConfirm = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='alertify-logs top right']")));
+        return deletePlaylistConfirm.getText();
+
+
+    }
+
+    public void clickDeleteBtn() {
+        WebElement deletePlaylistBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='del btn-delete-playlist']")));
+        deletePlaylistBtn.click();
+
+
+        WebElement confirmDeleteBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='ok']")));
+        confirmDeleteBtn.click();
+    }
+
 }
+
+
+
