dcdef165a04a5a38a6cc164e66f24600750682b1
Raul Barrios <barrau89@gmail.com>
2023-07-09 08:45:56 -0500

    HW18 @test code added

--- src/test/java/Homework_18.java
+++ src/test/java/Homework_18.java
@@ -1,22 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.*;
 
 public class Homework_18 extends  BaseTest{
     @Test
-    public void playSong() {
+    public void playSong() throws InterruptedException {
 
-        //  Open Koel Website
         BaseTest.openLoginUrl(url);
-        // Log-in to Koel
         BaseTest.enterEmail(userEmail);
         BaseTest.enterPassword(userPassword);
         BaseTest.clickSubmitButton();
-        // Search for a Song
-//        findSong("Dark Days");
+        clickNextSongButton();
+        clickPlaySongButton();
+        Assert.assertTrue(songIsPlaying());
 
     }
 
     String url = "https://qa.koel.app/";
     String userEmail= "barrau89@gmail.com";
     String userPassword = "te$t$tudent";
+
+    public void clickPlaySongButton() throws InterruptedException {
+        WebElement playButton = driver.findElement(By.cssSelector("span[data-testid='play-btn']"));
+        playButton.click();
+    }
+
+    public void clickNextSongButton() throws InterruptedException {
+        WebElement nextButton = driver.findElement(By.cssSelector("span[data-testid='play-next-btn']"));
+        nextButton.click();
+    }
+
+    public boolean songIsPlaying(){
+        WebElement soundbar = driver.findElement(By.cssSelector("div[data-testid='sound-bar-play']"));
+        return soundbar.isDisplayed();
+    }
+
 }
