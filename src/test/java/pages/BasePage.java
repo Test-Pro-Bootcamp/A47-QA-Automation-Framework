6ee37097bd7393da42319b643cfefaed8df4f7db
Matt Pierce <119891174+mattpierce017@users.noreply.github.com>
2023-07-13 12:46:45 -0400

    setting up Reusable elements in BasePage

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,6 +1,7 @@
 package pages;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.*;
 
@@ -10,4 +11,24 @@ public class BasePage {
     public static WebDriverWait wait = null;
     public static Actions actions = null;
     public static String url = "https://qa.koel.app/";
+
+    private WebElement findElement(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+    private void rightClick(By locator) {
+        return wait.until(ExpectedConditions.ElementIsClickable(locator));
+        locator.contextClick();
+    }
+    private void waitForOverlay(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+    private void doubleClick(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        locator.double.click();
+    }
 }
+
+}
+}
+
+
