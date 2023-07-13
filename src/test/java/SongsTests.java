f11825aa9d8c87c531a53955439751ed3b82c018
Matt Pierce <119891174+mattpierce017@users.noreply.github.com>
2023-07-13 12:26:21 -0400

    initialized BasePage

--- src/test/java/SongsTests.java
+++ src/test/java/SongsTests.java
@@ -1,8 +1,18 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
 
 public class SongsTests extends BaseTest{
     @Test
     private void playSong(){
+
+        LoginPage.login();
+        HomePage.chooseAllSongsList();
+        HomePage.contextClickFirstSong();
+        HomePage.choosePlayOption();
+
+        Assert.assertTrue(isSongPlaying());
 
     }
 
--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,7 +1,13 @@
 package pages;
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.*;
 
 
 public class BasePage {
+    public static WebDriver driver = null;
+    public static WebDriverWait wait = null;
+    public static Actions actions = null;
+    public static String url = "https://qa.koel.app/";
 }
