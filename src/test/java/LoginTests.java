6d88205fb8a91959ca784b6187791ae9db89b1c4
Gtandrea1993 <andrea.guevara@testpro.io>
2023-07-16 13:02:46 -0400

    Homework-16
    Done

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -14,12 +16,44 @@ public class LoginTests extends BaseTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
+
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+        // Open the URL for the web page on the Chrome Browser
         String url = "https://qa.koel.app/";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        // Put the email field inside the web page
+
+       WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+       emailField.click();
+       emailField.clear();
+       emailField.sendKeys("andrea.guevara@testpro.io");
+
+
+        // Put the password field inside the web page
+
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.sendKeys("te$t$tudent");
+
+        //click on the submit button
+        WebElement submitLogin = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitLogin.click();
+
+        // check if the user avatar is displaying
+
+        WebElement avatar = driver.findElement(By.cssSelector(".avatar"));
+        Assert.assertTrue(avatar.isDisplayed());
+
+        //Quit the browser
+
         driver.quit();
+
+
+       //  Assert.assertEquals(driver.getCurrentUrl(), url);
+       //   driver.quit();
     }
 }
