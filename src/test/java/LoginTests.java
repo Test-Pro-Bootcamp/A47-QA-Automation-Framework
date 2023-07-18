107fa3c78d1f71a2970d6a92ecec52d54824d58a
Ice-testpro <irene.perdon@testpro.io>
2023-07-18 16:02:55 -0500

    Update Log-in
    Update log-in result

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -21,6 +21,7 @@ public class LoginTests extends BaseTest {
 
         String url = "https://qa.koel.app/";
         driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
 //
 //        openLoginUrl();
 //        enterEmail("demo@class.com");
