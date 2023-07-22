adc96c0ab4078e2a22dca557b63f9abdb46dd782
neeg05 <neelam.gupta@testpro.io>
2023-07-22 01:03:57 -0400

    test

---
+++ src/test/java/Homewor21.java
@@ -0,0 +1,17 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homewor21 extends BaseTest{
+    String message ="Updated playlist";
+
+    @Test(dataProvider = "validCredentials")
+    public void renamePlaylist(String email, String password){
+        enterEmail(email);
+        enterPassword(password);
+        clickSubmit();
+        selectPlaylist();
+        selectEdit();
+        renamePlaylist("something NEW");
+        Assert.assertTrue(validateAction().contains(message));
+    }
+}
