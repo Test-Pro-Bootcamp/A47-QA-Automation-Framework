f2444628528ef3fe5072e74c33f58ebea013bb71
Bella132327 <aimee.woodside@testpro.io>
2023-07-27 06:20:48 -0500

    playSong() Test Case

---
+++ src/test/java/Homework18.java
@@ -0,0 +1,32 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework18 extends BaseTest {
+    @Test
+    public void addSongToPlaylist() throws InterruptedException {
+
+        String newSongAddedNotification = "Added 1 song into";
+
+        openLoginUrl();
+
+        enterEmail("aimee.woodside@testpro.io");
+
+        enterPassword("te$t$tudent13");
+
+        clickLogInbutton();
+
+        searchSong("beautiful");
+
+        clickViewAllSearchBtn();
+
+        clickFirstSongSearchResult();
+
+        clickAddToBtn();
+
+        selectPlayList();
+
+        getNotificationMessage();
+
+        Assert.assertTrue(getNotificationMessage().contains(newSongAddedNotification));
+    }
+}
