a544ab718cac68b3eca8718d59bd7eb7fd6e7494
Ice-testpro <irene.perdon@testpro.io>
2023-08-29 20:51:16 -0500

    Update error
    Error for findElement

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,46 +6,53 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
 
 public class BaseTest {
-       public static WebDriverManager driver = null;
-       public static String url = ("https://qa.koel.app/");
+    public static WebDriverManager driver = null;
+    public static String url = ("https://qa.koel.app/");
 
-       @BeforeSuite
-       static void setupClass() {
-           WebDriverManager.chromedriver().setup();
-       }
+    @BeforeSuite
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
 
-       @BeforeMethod
-       public void launchBrowser() {
-           ChromeOptions options = new ChromeOptions();
-           options.addArguments("--remote-allow-origins=*");
-           WebDriver driver = new ChromeDriver(options);
-           driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-       }
+    @BeforeMethod
+    public void launchBrowser() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
 
-       @AfterMethod
-       public void closeBrowser() {
-           driver.quit();
-       }
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
 
-       public void provideEmail(String email) {
-           WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
-           emailField.clear();
-           emailField.sendKeys(email);
-       }
+    protected static void openLoginUrl() {
+        String url = ("https://qa.koel.app/");
+        driver.get(url);
+    }
 
-       public void providePassword(String password) {
-           WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
-           passwordField.clear();
-           passwordField.sendKeys(password);
-       }
+    public void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.clear();
+        emailField.click();
+        emailField.sendKeys("irene.perdon@testpro.io");
+    }
 
-       public void clickSubmit() {
-           WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
-           submit.click();
-       }
-   }
+    public void providePassword(String password) {
+        WebElement passwordField = driver.getWebDriver(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.click();
+        passwordField.sendKeys("te$t$tudent");
+    }
+
+    public void clickSubmit() {
+        WebElement submit = driver.getWebDriver().findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+    }
+}
