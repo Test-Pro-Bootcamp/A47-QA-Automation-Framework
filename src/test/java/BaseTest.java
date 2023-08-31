cea244fd45dcdb42e00c93f0ee9debd1f4abb530
Gtandrea1993 <andrea.guevara@testpro.io>
2023-08-31 12:53:43 -0400

    Homework 21
    I have this issue with the launchBrowser, I thought all my code is okay : Please can you help me
    
    Could not start a new session. Response code 500. Message: session not created: This version of ChromeDriver only supports Chrome version 114
    Current browser version is 116.0.5845.140 with binary path C:\Program Files (x86)\Google\Chrome\Application\chrome.exe
    Host info: host: 'DESKTOP-L7LSH6V', ip: '192.168.1.251'
    Build info: version: '4.5.0', revision: 'fe167b119a'

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,92 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
+
+    public static WebDriver driver = null;
+    public static Actions actions = null;
+
+    public static WebDriverWait wait = null;
+    public static String url = "https://qa.koel.app/";
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+    @BeforeMethod
+    @Parameters({"BaseURL"})
+    public void launchBrowser(String BaseURL){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        url = BaseURL;
+        driver.get(url);
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        actions = new Actions(driver);
+
+    }
+
+
+    public static void enterEmail(String email){
+        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='email']")));
+        emailField.click();
+        emailField.clear();
+        emailField.sendKeys(email);
+
+    }
+
+    public static void enterPassword(String password){
+        WebElement passwordInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='password']")));
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+    }
+    public static void clickSubmit() {
+        WebElement submitLogin = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
+        submitLogin.click();
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    String newPlayListName = "Rename PlayList done";
+    public void doubleClickPlayList(){
+        WebElement selectPlayList = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+        actions.doubleClick(selectPlayList).perform();
+    }
+    public void enterNewPlayListName(){
+        WebElement playListInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
+        playListInputField.sendKeys(Keys.chord(Keys.CONTROL, "A", Keys.BACK_SPACE));
+
+        playListInputField.sendKeys(newPlayListName);
+        playListInputField.sendKeys(Keys.ENTER);
+    }
+
+    public String getRenamePlayListSuccessMsg(){
+        WebElement notification = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
+        return notification.getText();
+
+    }
+
 }
