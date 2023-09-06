b61a9fead5e56f12fcbd5c39941089f249d97971
Artur Harutyunyan <87441020+ArturH1990@users.noreply.github.com>
2023-09-05 21:57:24 -0700

    Login with empty password
    Successfully Login with empty password

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,7 @@
+import org.checkerframework.checker.units.qual.A;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -17,9 +20,29 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://qa.koel.app/";
+
+        String url ="https://qa.koel.app/" ;
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        WebElement emailIn = driver.findElement(By.cssSelector("[type='email']"));
+        emailIn.click();
+        emailIn.clear();
+        emailIn.sendKeys("demo@class.com");
+
+      /*  WebElement passwordIn = driver.findElement(By.cssSelector("[type='password']"));
+        passwordIn.click();
+        passwordIn.clear();
+        passwordIn.sendKeys("te$t$tudent");
+*/
+
+        WebElement submitB = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitB.click();
+
+
+        //  Assert.assertTrue(avatar.isDisplayed());
+
+        Assert.assertEquals(driver.getCurrentUrl(),url);
+        Assert.assertTrue(submitB.isDisplayed());
         driver.quit();
     }
 }
