8f2c4184fe85e0705bfda5e606fc1d040e511069
Artur Harutyunyan <87441020+ArturH1990@users.noreply.github.com>
2023-09-07 21:50:43 -0700

    Add Locations
    Adding the locations,
    finding elementes

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -6,7 +8,7 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LoginTests extends BaseTest {
+public class LoginTests {     //extends BaseTest {
     @Test
     public void LoginEmptyEmailPasswordTest() {
 
