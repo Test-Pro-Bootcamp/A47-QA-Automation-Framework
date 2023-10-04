e9a82de2996bade21d07957e6c4201662d0e1a0c
randyvergil <randy.davila@testpro.io>
2023-10-03 22:56:54 -0400

    1

---
+++ src/test/java/stepDefinition/LoginStepDefinitions.java
@@ -0,0 +1,16 @@
+package stepDefinition;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+
+public class LoginStepDefinitions {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @Given
+
+
+
+
+}
